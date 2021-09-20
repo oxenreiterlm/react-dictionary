1ff78729411d2d742b8806b06a7d873e99d6ddd2
oxenreiterlm <86936353+oxenreiterlm@users.noreply.github.com>
2021-09-20 18:19:57 -0400

    added search form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a href="/" className="btn btn-primary">
-          Hello
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Open-source code by Laura Oxenreiter
+        </footer>
+      </div>
     </div>
   );
 }
