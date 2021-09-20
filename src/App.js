36b7f735932fae14f70198d4cbad5e5f129fcfd3
oxenreiterlm <86936353+oxenreiterlm@users.noreply.github.com>
2021-09-20 11:34:11 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export defaul function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hello</header>
+    </div>
+  );
+}
+
+export default App;
