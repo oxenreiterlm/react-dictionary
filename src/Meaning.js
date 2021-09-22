4f2617077b3bf053622ec13ffdaf50192d3fa11b
oxenreiterlm <86936353+oxenreiterlm@users.noreply.github.com>
2021-09-21 21:49:52 -0400

    added phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,6 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h4 className="partOfSpeech">{props.meaning.partOfSpeech}</h4>
